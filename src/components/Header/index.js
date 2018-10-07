d2497d6179f4d71fce113b91c9fd779cbb1c0875
James Sullivan <james_sullivan@mckinsey.com>
2018-10-07 12:38:24 -0700

    Add header styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,23 +4,29 @@ import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => (
-  <nav className="header-main">
-    <NavLink activeClassName="nav-link-selected" exact to="/">
-      Home
-    </NavLink>
-    <NavLink activeClassName="nav-link-selected" to="/mentoring">
-      Mentoring
-    </NavLink>
-    <NavLink activeClassName="nav-link-selected" to="/volunteer">
-      Volunteering
-    </NavLink>
-    <NavLink activeClassName="nav-link-selected" to="/donate">
-      Donate
-    </NavLink>
-    <NavLink activeClassName="nav-link-selected" to="/about">
-      About Us
-    </NavLink>
-  </nav>
+  <header className="header-main">
+    <h1 className="logo">
+      <span className="accent-teal">re</span>
+      <span className="main-color">Parent</span>
+    </h1>
+    <nav>
+      <NavLink activeClassName="nav-link-selected" exact to="/">
+        Home
+      </NavLink>
+      <NavLink activeClassName="nav-link-selected" to="/mentoring">
+        Mentoring
+      </NavLink>
+      <NavLink activeClassName="nav-link-selected" to="/volunteer">
+        Volunteering
+      </NavLink>
+      <NavLink activeClassName="nav-link-selected" to="/donate">
+        Donate
+      </NavLink>
+      <NavLink activeClassName="nav-link-selected" to="/about">
+        About Us
+      </NavLink>
+    </nav>
+  </header>
 );
 
 export default Header;
