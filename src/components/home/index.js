a462d85366328287b588c178af4be18bb799a3f2
James Sullivan <james_sullivan@mckinsey.com>
2018-10-07 11:59:38 -0700

    add last minute styling changes

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -12,32 +12,33 @@ const Home = () => (
         <button className="button" type="button">Hear the Wise</button>
       </div>
     </div>
-    <div className ="div-color">
-      <h1 className = "center-main-text">MENTORING</h1>
-      <video className = "video" width="320" height="240" controls>
-      <source src="../../assets/interview-1.MOV" type="video/MOV"></source>
-    </video><br></br>
-    <button className="button-large" type="button">Learn More About Mentoring</button>
+    <div className="div-color">
+      <h1 className="center-main-text">MENTORING</h1>
+      <video className="video" width="320" height="240" controls>
+        <source src="../../assets/interview-1.MOV" type="video/MOV" />
+      </video>
+      <br />
+      <button className="button-large" type="button">Learn More About Mentoring</button>
     </div>
-    <div className="div">
+    <div className="div section-container">
       <div className="half-div">
         <img className="photo" src={photo} alt="Volunteer Today" />
       </div>
       <div className="half-div">
         <h2 className="center-text">Volunteer</h2>
         <p className="center-text">There are many ways you can contribute with this community’s ongoing success. Spread the word! Do you know somebody who is looking for advice or interested in mentoring? We can expand this kind and helpful community through people like you. Sign up to mentor or even help maintain this website. The possibilities are endless!</p>
+        <button className="button-large" type="button">Learn More About the Possibilities of Mentoring</button>
       </div>
-      <button className="button-large" type="button">Learn More About the Possibilities of Mentoring</button>
     </div>
-    <div className ="div-color">
-    <div className="half-div">
-      <h2 className="center-text">Donate</h2>
-      <p className="center-text">We aspire to bring together these different generations in a one on one digital environment, fostering mutually beneficial dialogues, where questions on specific topics are answered and personal connections are enhanced. </p>
-    </div>
-    <div className="half-div">
-      <img className="photo" src={photo2} alt="Volunteer Today" />
-    </div>
-    <button className="button-large" type="button">Learn More About the Possibilities of Mentoring</button>
+    <div className="div-color section-container">
+      <div className="half-div">
+        <h2 className="center-text">Donate</h2>
+        <p className="center-text">We aspire to bring together these different generations in a one on one digital environment, fostering mutually beneficial dialogues, where questions on specific topics are answered and personal connections are enhanced. </p>
+        <button className="button-large" type="button">Learn More About the Possibilities of Mentoring</button>
+      </div>
+      <div className="half-div">
+        <img className="photo" src={photo2} alt="Volunteer Today" />
+      </div>
     </div>
   </div>
 );
